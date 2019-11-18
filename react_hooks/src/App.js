fd2a3e7079cb608851ec63e8aea4be04c692d90d
Park Jongho <kim010c@gmail.com>
2019-11-18 15:25:25 +0900

    #2 useInput
    onChange and useState 사용법

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,15 +1,39 @@
 import React, { useState } from "react";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+    let willUpdate = true;
+
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+
+  return { value, onChange };
+};
+
 const App = () => {
   const [item, setItem] = useState(0);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
-
+  const maxLen = value => value.length <= 10;
+  const name = useInput("", maxLen);
   return (
-    <div className="App">
+    <div>
       <h1>useState 사용 : {item}</h1>
       <button onClick={incrementItem}> increment</button>
       <button onClick={decrementItem}> decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
