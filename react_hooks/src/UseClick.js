c585b6e37f423d8013230dd414d7ec5acbb22c49
Park Jongho <kim010c@gmail.com>
2019-11-18 21:32:20 +0900

    #5 useClick
    useRef사용법

---
+++ react_hooks/src/UseClick.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from "react";
+
+const useClick = onClick => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
+const UseClick = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div>
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+export default UseClick;
